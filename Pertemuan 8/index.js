270c84fd92d2ddd41266709a80a57af4e2eae61a
ValentinoOroh <valentinoorohtorres@gmail.com>
2020-09-24 10:19:40 +0800

    First Commit

--- Pertemuan 8/index.js
+++ Pertemuan 8/index.js
@@ -1,23 +1,25 @@
-let jumlah = 0;
-const account = {
+// let jumlah = 0;
+// const account = {
     
-    name : "Valentino",
-    expenses : []
+//     name : "Valentino",
+//     expenses : []
      
-};
-function addExpenses(description, amount)
-{
-  account.expenses.push({description : description, amount: amount})
-}
-function getAccountSummary()
-{
-    account.expenses.forEach(function(x)
-    {
-        jumlah = jumlah + x.amount
-    })
-    console.log("Total Harga Pembelian dari "+ account.name +" = Rp. "+jumlah);
-}
+// };
+// function addExpenses(description, amount)
+// {
+//   account.expenses.push({description : description, amount: amount})
+// }
+// function getAccountSummary()
+// {
+//     account.expenses.forEach(function(x)
+//     {
+//           jumlah = jumlah + x.amount
+//     })
+//     console.log("Total Harga Pembelian dari "+ account.expenses +" = Rp. "+jumlah);
+// }
 
-addExpenses("Skin Epic", 250000);
-addExpenses("Skin Elite", 75000);
-getAccountSummary();
+// addExpenses("Skin Epic", 250000);
+// addExpenses("Skin Elite", 75000);
+// getAccountSummary();
+
+
